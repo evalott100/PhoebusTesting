cc04ac1fe54ca3f0239641066289ca40a341ae30
Richard Cunningham <fye77278@pc0142.cs.diamond.ac.uk>
2024-11-13 13:17:02 +0000

    various images

--- our_phoebus.sh
+++ our_phoebus.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
-point="/scratch/fye77278/PhoebusTesting/settings.ini"
+point="/scratch/fye77278/Code/PhoebusTesting/settings.ini"
+#export EPICS_CA_SERVER_PORT=6064
+#export EPICS_CA_REPEATER_PORT=6065
 module load java/11.0.15
 /dls_sw/apps/phoebus/master/phoebus-product/phoebus.sh -settings $point "$@"
 
